3c5e4f3c22bbae0711e982f89cfe74297263a119
Davit Grigorian <84412379+DavitGrigorian@users.noreply.github.com>
2021-05-19 02:54:20 +0400

    Add files via upload

---
+++ HomeWork1.js
@@ -0,0 +1,28 @@
+
+// Task 1
+let Name = prompt("What's your name?") , Surname = prompt("What's your surname?")
+alert("Your name is " +Name+ " and your surname is " +Surname)
+
+// Task 2
+let NumberA = 20 , NumberB = 10
+console.log(NumberA+NumberB)
+console.log(NumberA-NumberB)
+console.log(NumberA*NumberB)
+console.log(NumberA/NumberB)
+console.log(NumberA%NumberB)
+
+// Task 3
+let NumberInInput1 = 78756 , NumberInInput2 = -122 , NumberInInput3 = 8 , NumberInInput4 = 0
+console.log(NumberInInput1%10)
+console.log(NumberInInput2%10)
+console.log(NumberInInput3%10)
+console.log(NumberInInput4%10)
+
+// Task 4
+let MyName = "Davit" ,  MyAge = "25"
+console.log("My name is " +MyName+ ".I am " +MyAge+ ".")
+
+// Task 5
+
+
+
